57043a13bd80bb97fd85598c75d933c0928eb526
Justin Keller <jdk370@gmail.com>
2017-09-16 18:01:42 -0400

    I have no idea who but the SQLite file won't download so removed references to birthdays

--- app/src/main/java/justinkeller/hilltop/SettingsActivity.java
+++ app/src/main/java/justinkeller/hilltop/SettingsActivity.java
@@ -4,12 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.content.SharedPreferences;
-import android.preference.Preference;
-import android.support.annotation.Nullable;
 import android.widget.EditText;
 import android.widget.Toast;
 import android.preference.PreferenceManager;
-import android.view.View.OnFocusChangeListener;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,12 +21,12 @@ public class SettingsActivity extends Activity implements OnSharedPreferenceChan
         setContentView(R.layout.settings_activity);
         final EditText currentURL=findViewById(R.id.configurl);
         final EditText homeTime=findViewById(R.id.timeinput);
-       final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         currentURL.setText(prefs.getString("configURL","http://hilltopprep.org"));
         homeTime.setText(prefs.getString("homeTime","17:00:00"));
         Button submitTime=findViewById(R.id.submittime);
         Button submit=findViewById(R.id.subbutton);
-        submit.setOnClickListener(new View.OnClickListener() {
+   /*     submit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putString("configURL",currentURL.getText().toString()); // value to store
@@ -38,7 +35,7 @@ public class SettingsActivity extends Activity implements OnSharedPreferenceChan
                 String download = sharedPreferences.getString("configurl","nullstring");
                 Toast.makeText(getApplicationContext(),download.toString(),Toast.LENGTH_LONG);
             }
-        });
+        }); */
         submitTime.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 SharedPreferences.Editor editor = prefs.edit();
@@ -46,16 +43,16 @@ public class SettingsActivity extends Activity implements OnSharedPreferenceChan
                 editor.commit();
                 SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 String download = sharedPreferences.getString("homeTime","nullstring");
-                Toast.makeText(getApplicationContext(),download.toString(),Toast.LENGTH_LONG);
+                Toast.makeText(getApplicationContext(),download,Toast.LENGTH_LONG).show();
             }
         });
 
 
-        }
+    }
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
-        if (key.equals("configURL")) {
-            String download = sharedPreferences.getString(key,"nullstring");
-            Toast.makeText(this,download,Toast.LENGTH_LONG);
+        if (key.equals("homeTime")) {
+            String homeTime = sharedPreferences.getString(key,"nullstring");
+            Toast.makeText(this,homeTime,Toast.LENGTH_LONG).show();
         }
     }
     public void onClick(View v) {
@@ -63,4 +60,4 @@ public class SettingsActivity extends Activity implements OnSharedPreferenceChan
 
         }
     }
-    }
+}
