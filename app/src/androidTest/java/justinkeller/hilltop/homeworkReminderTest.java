484c947c7355a34840ff2c5de005e7104ada524a
Justin Keller <jdk370@gmail.com>
2017-10-01 14:55:28 -0400

    misc

---
+++ app/src/androidTest/java/justinkeller/hilltop/homeworkReminderTest.java
@@ -0,0 +1,34 @@
+package justinkeller.hilltop;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+/**
+ * Created by jkeller on 9/16/17.
+ */
+public class homeworkReminderTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+
+    @Test
+    public void onCreate() throws Exception {
+        assertNotNull(new homeworkReminder().myRemTime);
+    }
+
+    @Test
+    public void addReminder() throws Exception {
+
+    }
+
+}
+
