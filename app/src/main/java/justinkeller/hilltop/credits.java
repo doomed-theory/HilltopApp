f3400ebde03468f0174d90630af829bfa5e330b4
Justin Keller <jdk370@gmail.com>
2017-10-06 17:07:27 -0400

    style

---
+++ app/src/main/java/justinkeller/hilltop/credits.java
@@ -0,0 +1,13 @@
+package justinkeller.hilltop;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class credits extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+}
